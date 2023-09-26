9d3a4285484bade1c5a2457ed90378f46ef496ee
tdbrns <w10051624@usm.edu>
2023-09-25 19:07:29 -0500

    Revert "Revert "update to functions""
    This reverts commit 4e15df536f4d1fa2577bb48eccc7e4d017dc0ee1.

--- CSC414Team2_CryptopalsSoftwareSuite/MainForm.h
+++ CSC414Team2_CryptopalsSoftwareSuite/MainForm.h
@@ -1,152 +1,221 @@
 #pragma once
-
-namespace CSC414Team2CryptopalsSoftwareSuite {
-
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
-
-	/// <summary>
-	/// Summary for MainForm
-	/// </summary>
-	public ref class MainForm : public System::Windows::Forms::Form
-	{
-	public:
-		MainForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
-
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~MainForm()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
-	private: System::Windows::Forms::Label^ lblTitle;
-
-	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
-	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel2;
-	private: System::Windows::Forms::Label^ lblCredits;
+#include <msclr/marshal_cppstd.h>
 
 
+namespace Project17 {
 
-	protected:
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
 
+    /// <summary>
+    /// Summary for MyForm
+    /// </summary>
+    public ref class MyForm : public System::Windows::Forms::Form
+    {
+    private:
+        String^ base_hex_array;
+    private: System::Windows::Forms::PictureBox^ background_img;
+        String^ base_64_array;
+    public:
+        MyForm(void)
+        {
+            InitializeComponent();
+            base_hex_array = {};
+            base_64_array = {};
 
-	protected:
+            this->background_img->SizeMode = PictureBoxSizeMode::StretchImage;
+            this->background_img->Dock = DockStyle::Fill;
+            try {
+                this->background_img->Image = Image::FromFile("C:/Users/ticau/source/repos/Project17/pic.jpg"); // Set the path to your image file
+            }
+            catch (Exception^ ex) {
+                MessageBox::Show("Error loading image: " + ex->Message);
+            }
+            //
+            //TODO: Add the constructor code here
+            //
+        }
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+    protected:
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        ~MyForm()
+        {
+            if (components)
+            {
+                delete components;
+            }
+        }
+        // The PerformConversion() function you provided
+        void MyForm::PerformConversion() {
+            // Check if base_hex_array is not empty
+            if (!String::IsNullOrEmpty(base_hex_array)) {
+                // Convert System::String to std::string
+                System::String^ managedString = base_hex_array;
+                std::string hexString = msclr::interop::marshal_as<std::string>(managedString);
+
+                // Task 1: Convert hex to bytes using the function from CryptoOperations.cpp
+                std::vector<unsigned char> bytes = hexToBytes(hexString);
+
+                // Task 2: Convert bytes to base64 using the function from CryptoOperations.cpp
+                std::string base64String = bytesToBase64(bytes);
+
+                // Update the base_64_array member (optional)
+                base_64_array = gcnew String(base64String.c_str());
+
+                // Update the hash_output text box with the base64 result
+                hash_output->Text = gcnew String(base64String.c_str());
+            }
+            else {
+                // Handle the case where base_hex_array is empty
+                hash_output->Text = "Input is empty.";
+            }
+        }
+    private: System::Windows::Forms::TextBox^ hash_input;
+    private: System::Windows::Forms::TextBox^ hash_output;
+    private: System::Windows::Forms::Button^ Convert;
+    private: System::Windows::Forms::Button^ Cancel;
+    private: System::Windows::Forms::Label^ label1;
+    private: System::Windows::Forms::Label^ label2;
+    protected:
+
+    private:
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->lblTitle = (gcnew System::Windows::Forms::Label());
-			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
-			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
-			this->lblCredits = (gcnew System::Windows::Forms::Label());
-			this->tableLayoutPanel1->SuspendLayout();
-			this->tableLayoutPanel2->SuspendLayout();
-			this->SuspendLayout();
-			// 
-			// lblTitle
-			// 
-			this->lblTitle->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Left | System::Windows::Forms::AnchorStyles::Right));
-			this->lblTitle->AutoSize = true;
-			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lblTitle->Location = System::Drawing::Point(3, 3);
-			this->lblTitle->Name = L"lblTitle";
-			this->lblTitle->Size = System::Drawing::Size(793, 25);
-			this->lblTitle->TabIndex = 0;
-			this->lblTitle->Text = L"Cryptopals Software Suite";
-			this->lblTitle->TextAlign = System::Drawing::ContentAlignment::TopCenter;
-			this->lblTitle->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
-			// 
-			// tableLayoutPanel1
-			// 
-			this->tableLayoutPanel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->tableLayoutPanel1->ColumnCount = 1;
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				50)));
-			this->tableLayoutPanel1->Controls->Add(this->lblTitle, 0, 0);
-			this->tableLayoutPanel1->Location = System::Drawing::Point(12, 12);
-			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
-			this->tableLayoutPanel1->RowCount = 1;
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(799, 31);
-			this->tableLayoutPanel1->TabIndex = 1;
-			// 
-			// tableLayoutPanel2
-			// 
-			this->tableLayoutPanel2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->tableLayoutPanel2->ColumnCount = 1;
-			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				50)));
-			this->tableLayoutPanel2->Controls->Add(this->lblCredits, 0, 0);
-			this->tableLayoutPanel2->Location = System::Drawing::Point(12, 43);
-			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
-			this->tableLayoutPanel2->RowCount = 1;
-			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel2->Size = System::Drawing::Size(799, 31);
-			this->tableLayoutPanel2->TabIndex = 1;
-			// 
-			// lblCredits
-			// 
-			this->lblCredits->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Left | System::Windows::Forms::AnchorStyles::Right));
-			this->lblCredits->AutoSize = true;
-			this->lblCredits->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lblCredits->Location = System::Drawing::Point(3, 3);
-			this->lblCredits->Name = L"lblCredits";
-			this->lblCredits->Size = System::Drawing::Size(793, 25);
-			this->lblCredits->TabIndex = 0;
-			this->lblCredits->Text = L"By CSC 414 Team 2";
-			this->lblCredits->TextAlign = System::Drawing::ContentAlignment::TopCenter;
-			this->lblCredits->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
-			// 
-			// MainForm
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(823, 483);
-			this->Controls->Add(this->tableLayoutPanel2);
-			this->Controls->Add(this->tableLayoutPanel1);
-			this->Name = L"MainForm";
-			this->Text = L"Main Menu";
-			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
-			this->tableLayoutPanel1->ResumeLayout(false);
-			this->tableLayoutPanel1->PerformLayout();
-			this->tableLayoutPanel2->ResumeLayout(false);
-			this->tableLayoutPanel2->PerformLayout();
-			this->ResumeLayout(false);
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        void InitializeComponent(void)
+        {
+            this->hash_input = (gcnew System::Windows::Forms::TextBox());
+            this->hash_output = (gcnew System::Windows::Forms::TextBox());
+            this->Convert = (gcnew System::Windows::Forms::Button());
+            this->Cancel = (gcnew System::Windows::Forms::Button());
+            this->label1 = (gcnew System::Windows::Forms::Label());
+            this->label2 = (gcnew System::Windows::Forms::Label());
+            this->background_img = (gcnew System::Windows::Forms::PictureBox());
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->background_img))->BeginInit();
+            this->SuspendLayout();
+            //
+            // hash_input
+            //
+            this->hash_input->Location = System::Drawing::Point(12, 60);
+            this->hash_input->Name = L"hash_input";
+            this->hash_input->Size = System::Drawing::Size(176, 20);
+            this->hash_input->TabIndex = 0;
+            this->hash_input->TextChanged += gcnew System::EventHandler(this, &MyForm::hash_input_TextChanged);
+            //
+            // hash_output
+            //
+            this->hash_output->Location = System::Drawing::Point(12, 23);
+            this->hash_output->Name = L"hash_output";
+            this->hash_output->Size = System::Drawing::Size(176, 20);
+            this->hash_output->TabIndex = 1;
+            this->hash_output->TextChanged += gcnew System::EventHandler(this, &MyForm::hash_output_TextChanged);
+            //
+            // Convert
+            //
+            this->Convert->ForeColor = System::Drawing::SystemColors::ControlText;
+            this->Convert->Location = System::Drawing::Point(12, 96);
+            this->Convert->Name = L"Convert";
+            this->Convert->Size = System::Drawing::Size(75, 23);
+            this->Convert->TabIndex = 2;
+            this->Convert->Text = L"Convert";
+            this->Convert->UseVisualStyleBackColor = true;
+            this->Convert->Click += gcnew System::EventHandler(this, &MyForm::hash_Click);
+            //
+            // Cancel
+            //
+            this->Cancel->Location = System::Drawing::Point(113, 96);
+            this->Cancel->Name = L"Cancel";
+            this->Cancel->Size = System::Drawing::Size(75, 23);
+            this->Cancel->TabIndex = 3;
+            this->Cancel->Text = L"Cancel";
+            this->Cancel->UseVisualStyleBackColor = true;
+            this->Cancel->Click += gcnew System::EventHandler(this, &MyForm::Cancel_Click);
+            //
+            // label1
+            //
+            this->label1->AutoSize = true;
+            this->label1->BackColor = System::Drawing::SystemColors::ButtonFace;
+            this->label1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+            this->label1->Location = System::Drawing::Point(12, 7);
+            this->label1->Name = L"label1";
+            this->label1->Size = System::Drawing::Size(53, 13);
+            this->label1->TabIndex = 4;
+            this->label1->Text = L"Base Hex";
+            this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+            //
+            // label2
+            //
+            this->label2->AutoSize = true;
+            this->label2->Location = System::Drawing::Point(12, 46);
+            this->label2->Name = L"label2";
+            this->label2->Size = System::Drawing::Size(46, 13);
+            this->label2->TabIndex = 5;
+            this->label2->Text = L"Base 64";
+            //
+            // background_img
+            //
+            this->background_img->Location = System::Drawing::Point(-1, -5);
+            this->background_img->Name = L"background_img";
+            this->background_img->Size = System::Drawing::Size(286, 136);
+            this->background_img->TabIndex = 6;
+            this->background_img->TabStop = false;
+            this->background_img->Click += gcnew System::EventHandler(this, &MyForm::background_img_Click);
+            //
+            // MyForm
+            //
+            this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->ClientSize = System::Drawing::Size(280, 127);
+            this->Controls->Add(this->label2);
+            this->Controls->Add(this->label1);
+            this->Controls->Add(this->Cancel);
+            this->Controls->Add(this->hash_output);
+            this->Controls->Add(this->hash_input);
+            this->Controls->Add(this->Convert);
+            this->Controls->Add(this->background_img);
+            this->Name = L"MyForm";
+            this->Text = L"CryptoPal";
+            this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->background_img))->EndInit();
+            this->ResumeLayout(false);
+            this->PerformLayout();
 
-		}
+        }
 #pragma endregion
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	};
-}
+    private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+        }
+    private: System::Void hash_Click(System::Object^ sender, System::EventArgs^ e) {
+            base_hex_array = hash_input->Text;
+        }
+    private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+        }
+    private: System::Void hash_output_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+        }
+    private: System::Void Cancel_Click(System::Object^ sender, System::EventArgs^ e) {
+            hash_input->Text = "";    // Clear the input text box
+            hash_output->Text = "";   // Clear the output text box
+        }
+    private: System::Void background_img_Click(System::Object^ sender, System::EventArgs^ e) {
+
+        }
+
+    private: System::Void hash_input_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+        }
+    };
+};
+
+
--- CSC414Team2_CryptopalsSoftwareSuite/Source.cpp
+++ CSC414Team2_CryptopalsSoftwareSuite/Source.cpp
@@ -1,10 +1,86 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-using namespace std;
+std::vector<unsigned char> hexToBytes(const std::string& hex);
+std::string bytesToBase64(const std::vector<unsigned char>& bytes);
+std::string hexXOR(const std::string& hex1, const std::string& hex2);
 
-int main()
-{
+int main() {
+    // Task 1: Convert hex to bytes
+    std::string hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    std::vector<unsigned char> bytes = hexToBytes(hexString);
 
-	return 0;
+    // Task 2: Convert bytes to base64
+    std::string base64String = bytesToBase64(bytes);
+    std::cout << "Base64 Encoding: " << base64String << std::endl;
+
+    // Task 3: Perform fixed XOR operation
+    std::string hexString1 = "1c0111001f010100061a024b53535009181c";
+    std::string hexString2 = "686974207468652062756c6c277320657965";
+    std::string xorResult = hexXOR(hexString1, hexString2);
+    std::cout << "XOR Result: " << xorResult << std::endl;
+
+    return 0;
+}
+
+std::vector<unsigned char> hexToBytes(const std::string& hex) {
+    std::vector<unsigned char> bytes;
+
+    for (size_t i = 0; i < hex.length(); i += 2) {
+        std::string byteString = hex.substr(i, 2);
+        unsigned char byte = static_cast<unsigned char>(std::stoi(byteString, nullptr, 16));
+        bytes.push_back(byte);
+    }
+
+    return bytes;
+}
+
+
+
+std::string bytesToBase64(const std::vector<unsigned char>& bytes) {
+    static const std::string base64Chars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789+/";
+
+    std::string base64String;
+    size_t i = 0;
+
+    while (i < bytes.size()) {
+        unsigned char byte1 = bytes[i++];
+        unsigned char byte2 = (i < bytes.size()) ? bytes[i++] : 0;
+        unsigned char byte3 = (i < bytes.size()) ? bytes[i++] : 0;
+
+        unsigned int threeBytes = (byte1 << 16) | (byte2 << 8) | byte3;
+
+        base64String.push_back(base64Chars[(threeBytes >> 18) & 0x3F]);
+        base64String.push_back(base64Chars[(threeBytes >> 12) & 0x3F]);
+        base64String.push_back(base64Chars[(threeBytes >> 6) & 0x3F]);
+        base64String.push_back(base64Chars[threeBytes & 0x3F]);
+    }
+
+    // Add padding characters if necessary
+    while (base64String.length() % 4 != 0) {
+        base64String.push_back('=');
+    }
+
+    return base64String;
+}
+
+std::string hexXOR(const std::string& hex1, const std::string& hex2) {
+    if (hex1.length() != hex2.length()) {
+        return ""; // Buffers must be of equal length
+    }
+
+    std::string result;
+
+    for (size_t i = 0; i < hex1.length(); i++) {
+        unsigned char byte1 = hexToBytes(hex1.substr(i, 2))[0];
+        unsigned char byte2 = hexToBytes(hex2.substr(i, 2))[0];
+        unsigned char xorResult = byte1 ^ byte2;
+        result += "0123456789ABCDEF"[xorResult];
+    }
+
+    return result;
 }
